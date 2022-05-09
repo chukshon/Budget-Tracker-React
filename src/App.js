5ea38b284140b37edd725d9db836d46d2769832a
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-09 11:25:45 +0100

    budgetCard component temporarily done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Button, Stack } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+import BudgetCard from './components/BudgetCard'
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <Container>
+      <Stack direction='horizontal' gap='2' className='mb-4'>
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start',
+        }}
+      >
+        <BudgetCard name='Entertainment' amount={200} max={1000} />
+      </div>
+    </Container>
+  )
 }
 
 export default App
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const currencyFormatter = new Intl.NumberFormat(undefined, {
+  currency: 'Ngn',
+  style: 'currency',
+  minimumFractionDigits: 0,
+})
